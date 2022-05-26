2dd76586513c92dcf78d570884cdd756f1b83dcc
thedrummonger <drummerxtmail@gmail.com>
2022-05-26 14:13:14 -0500

    Added the ability to open the application with a console output by adding console as a cmd arg

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,15 +14,42 @@ namespace SlideShowMaker2
 {
     static class Program
     {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
+
+        [DllImport("kernel32", SetLastError = true)]
+        static extern bool AttachConsole(int dwProcessId);
+        private const int ATTACH_PARENT_PROCESS = -1;
+
+        [DllImport("kernel32.dll")]
+        static extern int AllocConsole();
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern bool FreeConsole();
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            IntPtr consoleWindow = GetForegroundWindow();
+            GetWindowThreadProcessId(consoleWindow, out int prId);
+            Process process = Process.GetProcessById(prId);
+
+            if(Environment.GetCommandLineArgs().Count() > 1 && Environment.GetCommandLineArgs().Any(x => x.ToLower() == "console"))
+            {
+                AttachConsole(-1);
+                process.Kill();
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
         }
+
     }
 }
